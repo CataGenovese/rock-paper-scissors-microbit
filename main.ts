39707cab6e69c4ce1929613261cbfaa7575e125d
Catastrófica <catalina.genovese.7e8@itb.cat>
2025-11-07 15:32:26 +0100

    Actualizar main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,16 +1,7 @@
-let hand = 0
+let ma = 0
 input.onGesture(Gesture.Shake, function () {
-    hand = randint(1, 3)
-    if (hand == 1) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        music.playSoundEffect("D D B E A F G E ", SoundExpressionPlayMode.UntilDone)
-    } else if (hand == 2) {
+    ma = randint(1, 3)
+    if (ma == 1) {
         basic.showLeds(`
             . . . . .
             . # # # .
@@ -18,7 +9,14 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 4120, 1266, 255, 148, 500, SoundExpressionEffect.Warble, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    } else if (ma == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     } else {
         basic.showLeds(`
             # # . . #
@@ -27,6 +25,5 @@ input.onGesture(Gesture.Shake, function () {
             # # . # .
             # # . . #
             `)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 1177, 4967, 0, 206, 266, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     }
 })
