5cbc50fe96fc6f681920cd955a3cc07d494b4648
Catastrófica <catalina.genovese.7e8@itb.cat>
2025-11-07 16:32:06 +0100

    jugador vs cpu

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
+let jugador = 0
 let ma = 0
+input.onButtonPressed(Button.A, function () {
+    jugador = 1
+    basic.showLeds(`
+        . . . . #
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+})
 input.onGesture(Gesture.Shake, function () {
     ma = randint(1, 3)
     if (ma == 1) {
@@ -26,4 +37,31 @@ input.onGesture(Gesture.Shake, function () {
             # # . . #
             `)
     }
+    if (jugador == ma) {
+        basic.showString("EMPAT")
+    } else if (jugador == 1 && ma == 3 || jugador == 2 && ma == 1 || jugador == 3 && ma == 2) {
+        basic.showString("has guanyat!")
+    } else {
+        basic.showString("has perdut!")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    jugador = 3
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    jugador = 2
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
